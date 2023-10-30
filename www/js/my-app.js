bb1e5148d7e1b7a3ad2a0e1f46ea0628c83e811c
Pilar Fernández <pilarfernandezc98@gmail.com>
2023-10-30 09:45:06 -0300

    Agrego funcionalidades de registro y login con Firebase

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,10 +14,9 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
+      {path: '/about/', url: 'about.html'},
+      {path: '/login/', url: 'login.html'},
+      {path: '/index/', url: 'index.html'},
     ]
     // ... other parameters
   });
@@ -36,8 +35,70 @@ $$(document).on('page:init', function (e) {
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+  $$("#btnRegistro").on("click", funcionRegistro);
+})
+
+$$(document).on('page:init', '.page[data-name="login"]', function (e) {
+  $$("#btnLogin").on("click", funcionLogin);
+})
+
 $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
     alert('Hello');
 })
+
+
+
+
+// FUNCIONES
+
+function funcionRegistro () {
+  email = $$("#emailIndex").val();
+  contrasena = $$("#contrasenaIndex").val();
+
+  if (email != "" && contrasena != "") {
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("Bienvenid@!!! " + email);
+      // ...
+      // mainView.router.navigate('/registro/');
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.error(errorCode);
+      console.error(errorMessage);
+      if (errorCode == "auth/email-already-in-use") {
+        console.error("El email ya se encuentra registrado");
+        }
+        // ..
+    });
+  }
+}
+
+function funcionLogin () {
+  email = $$("#emailLogin").val();
+  contrasena = $$("#contrasenaLogin").val();
+
+  if (email != "" && contrasena != "") {
+    firebase.auth().signInWithEmailAndPassword(email, contrasena)
+    .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+
+    console.log("Bienvenid@!!! " + email);
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    console.error(errorCode);
+    console.error(errorMessage);
+  });
+  }
+}
